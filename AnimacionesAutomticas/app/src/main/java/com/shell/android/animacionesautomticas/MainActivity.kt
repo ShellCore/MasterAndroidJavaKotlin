5788a71d89f73a11bddfa355a144bbf7b07236c2
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-09-04 15:13:50 -0500

    Animación de transición con fade

--- AnimacionesAutomticas/app/src/main/java/com/shell/android/animacionesautomticas/MainActivity.kt
+++ AnimacionesAutomticas/app/src/main/java/com/shell/android/animacionesautomticas/MainActivity.kt
@@ -2,11 +2,22 @@ package com.shell.android.animacionesautomticas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun cambiarVisibilidad(view: View) {
+        txtInfo.apply {
+            when (visibility) {
+                View.VISIBLE -> visibility = View.GONE
+                else -> visibility = View.VISIBLE
+            }
+        }
     }
 }
