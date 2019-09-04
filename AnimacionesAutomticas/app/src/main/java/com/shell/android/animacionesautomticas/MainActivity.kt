6f5fa1a8c6a3fd8097755e11ead2b06d07b82c39
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-09-04 17:17:10 -0500

    Ejemplo de animación de navegación entre activities

--- AnimacionesAutomticas/app/src/main/java/com/shell/android/animacionesautomticas/MainActivity.kt
+++ AnimacionesAutomticas/app/src/main/java/com/shell/android/animacionesautomticas/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.shell.android.animacionesautomticas
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -8,5 +11,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun gotoSecondActivity(view: View) {
+        val options = ActivityOptions.makeSceneTransitionAnimation(this)
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent, options.toBundle())
     }
 }
