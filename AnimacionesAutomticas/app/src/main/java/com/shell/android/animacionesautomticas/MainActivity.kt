fa3b1127b6389b7781b3b9d779c7c8655a346e03
Cesar Morales Garduño <shell.one.core@gmail.com>
2019-09-04 16:31:24 -0500

    Ejemplo de carga progresiva del contenido

--- AnimacionesAutomticas/app/src/main/java/com/shell/android/animacionesautomticas/MainActivity.kt
+++ AnimacionesAutomticas/app/src/main/java/com/shell/android/animacionesautomticas/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.shell.android.animacionesautomticas
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,17 +10,37 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var animationDrawable : AnimationDrawable? = null
+    private var animationDuration: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imgCheck.setBackgroundResource(R.drawable.animation_check)
-        animationDrawable = imgCheck.background as AnimationDrawable
+        animationDuration = resources.getInteger(android.R.integer.config_longAnimTime)
 
-        imgCheck.setOnClickListener {
-            animationDrawable!!.start()
+        txtContent.visibility = View.GONE
+        progressBar.setOnClickListener {
+            crossfadeAnimation()
         }
+    }
+
+    private fun crossfadeAnimation() {
+        txtContent.apply {
+            alpha = 0f
+            visibility = View.VISIBLE
+        }
+
+        txtContent.animate()
+            .alpha(1f)
+            .setDuration(animationDuration.toLong())
+
+        progressBar.animate()
+            .alpha(0f)
+            .setDuration(animationDuration.toLong())
+            .setListener(object : AnimatorListenerAdapter() {
+                override fun onAnimationEnd(animation: Animator?) {
+                    progressBar.visibility = View.GONE
+                }
+            })
     }
 }
